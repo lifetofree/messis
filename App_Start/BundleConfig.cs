f405e175cb0a8371b0394ef4b64ea76c58d44e2e
lifetofree <chonlaphon@gmail.com>
2015-06-21 22:28:33 +0700

    update MESSIS layout

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -21,11 +21,13 @@ namespace MESSIS
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/custom.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/custom.css"));
         }
     }
 }
